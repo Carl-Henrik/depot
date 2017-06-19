bce2457aa3f048ca989ed5ee61320d122961b7ba
Carl-Henrik <carl-henrik.perssonn@gmail.com>
2017-06-19 12:30:56 +0200

    db created

--- db/migrate/20170619100919_create_products.rb
+++ db/migrate/20170619100919_create_products.rb
@@ -3,8 +3,8 @@ class CreateProducts < ActiveRecord::Migration[5.0]
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.String :image_url
-      t.decimal :price
+      t.string :image_url
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
